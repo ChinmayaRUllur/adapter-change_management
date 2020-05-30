5414b6cbfc9fdf521af8f6690747acc414fc142d
ChinmayaRUllur <Chinmaya.Ullur@centurylink.com>
2020-05-30 12:53:02 +0000

    Lab 6

--- lab05.main.js
+++ lab05.main.js
@@ -94,53 +94,9 @@
 	* that handles the response.
 	*/
 	healthcheck(callback) {
-	this.getRecord((result, error) => {
-	/**
-	* For this lab, complete the if else conditional
-	* statements that check if an error exists
-	* or the instance was hibernating. You must write
-	* the blocks for each branch.
-	*/
-	let callbackData = null;
-	
-	let callbackError = null;
-	
-	if (error) {
-	/**
-	* Write this block.
-	* If an error was returned, we need to emit OFFLINE.
-	* Log the returned error using IAP's global log object
-	* at an error severity. In the log message, record
-	* this.id so an administrator will know which ServiceNow
-	* adapter instance wrote the log message in case more
-	* than one instance is configured.
-	* If an optional IAP callback function was passed to
-	* healthcheck(), execute it passing the error seen as an argument
-	* for the callback's errorMessage parameter.
-	*/
-	this.emitOffline();
-	
-	log.info('Service now adapter is offline {this.id}');
-	
-	callbackError = error;
-	} else {
-	/**
-	* Write this block.
-	* If no runtime problems were detected, emit ONLINE.
-	* Log an appropriate message using IAP's global log object
-	* at a debug severity.
-	* If an optional IAP callback function was passed to
-	* healthcheck(), execute it passing this function's result
-	* parameter as an argument for the callback function's
-	* responseData parameter.
-	*/
+	// We will build this method in a later lab. For now, it will emulate
+	// a healthy integration by emmitting ONLINE.
 	this.emitOnline();
-	
-	callbackData = result;
-	
-	log.info('Service now adapter is online');
-	}
-	});
 	}
 	
 	/**
@@ -221,4 +177,3 @@
 	}
 	
 	module.exports = ServiceNowAdapter;
-
